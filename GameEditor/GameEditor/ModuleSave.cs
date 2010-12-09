0b8bf750ee9ad5123551ba7111818119881473c3
tosrees <tosrees@c7e7341f-4038-4d2c-49e7-fada537707ad>
2010-12-09 03:20:51 +0000

    + added export, import, save module

---
+++ GameEditor/GameEditor/ModuleSave.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace GameEditor
+{
+    public class ModuleSave
+    {
+        public static bool Save(CImage image, List<CModule> moduleList, List<CFrame> frameList, List<CAnimation> animationList)
+        {
+            TextWriter textWriter = new StreamWriter(@"C:\image.xml");
+
+            XmlSerializer serializerImage = new XmlSerializer(typeof(CImage));
+            serializerImage.Serialize(textWriter, image);
+
+            XmlSerializer serializerModule = new XmlSerializer(typeof(List<CModule>));
+            serializerModule.Serialize(textWriter, moduleList);
+
+            XmlSerializer serializerFrame = new XmlSerializer(typeof(List<CFrame>));
+            serializerFrame.Serialize(textWriter, frameList);
+
+            XmlSerializer serializerAnimation = new XmlSerializer(typeof(List<CAnimation>));
+            serializerAnimation.Serialize(textWriter, animationList);
+
+            textWriter.Close();
+            return true;
+        }
+    }
+}
