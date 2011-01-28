ef9b7630fbd027fdbadd817326d1905696ff5ddf
tosrees <tosrees@c7e7341f-4038-4d2c-49e7-fada537707ad>
2011-01-28 18:31:46 +0000

    + data for opengles sample

---
+++ Samples/OpenGLES/DrawSpriteGLES/data/sprite.h
@@ -0,0 +1,18 @@
+#ifndef _SPRITE_H_
+#define _SPRITE_H_
+#define SPRITE_IMAGE            "sprite.bmp"
+#define SPRITE_MODULE_ID_MODULE_ID_0            (0)
+#define SPRITE_MODULE_ID_MODULE_ID_1            (1)
+#define SPRITE_MODULE_ID_MODULE_ID_2            (2)
+#define SPRITE_MODULE_ID_MODULE_ID_3            (3)
+#define SPRITE_MODULE_ID_MODULE_ID_4            (4)
+#define SPRITE_MODULE_ID_MODULE_ID_5            (5)
+#define SPRITE_FRAME_ID_FRAME_ID_0            (0)
+#define SPRITE_FRAME_ID_FRAME_ID_1            (1)
+#define SPRITE_FRAME_ID_FRAME_ID_2            (2)
+#define SPRITE_FRAME_ID_FRAME_ID_3            (3)
+#define SPRITE_FRAME_ID_FRAME_ID_4            (4)
+#define SPRITE_FRAME_ID_FRAME_ID_5            (5)
+#define SPRITE_FRAME_ID_FRAME_ID_6            (6)
+#define SPRITE_ANIMATION_ID_ANIMATION_ID_0            (0)
+#endif //HEADER_H
