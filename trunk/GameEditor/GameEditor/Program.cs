f4795a8d0cb7623ecd134ae8205fe39e7990d536
tosrees <tosrees@c7e7341f-4038-4d2c-49e7-fada537707ad>
2011-02-03 04:04:31 +0000

    * File association completed

--- trunk/GameEditor/GameEditor/Program.cs
+++ trunk/GameEditor/GameEditor/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
+using Org.Mentalis.Utilities;
+
 namespace GameEditor
 {
     static class Program
@@ -11,8 +13,27 @@ namespace GameEditor
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            ///////////////// ASSOCIATE IT WITH REGISTRY ////////////////////
+            string fileName = null;
+            for (int i = 0; i < args.Length; i++)
+            {
+                fileName = args[i];
+            }
+            GameEditor.m_GfxFilePath = fileName;
+
+            string path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            FileAssociation FA = new FileAssociation();
+            FA.Extension = "gfx";
+            FA.ContentType = "application/GameEditor";
+            FA.FullName = "GFX Files!";
+            FA.ProperName = "GFX File";
+            FA.AddCommand("open", path + " %1");
+            FA.IconPath = path;
+            FA.IconIndex = 0;       //Icon data in exe always ate the begining with index 0
+            FA.Create();
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new GameEditor());
