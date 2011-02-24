7dba1bcf46e1741c3b3dd6a03fc78390fd238270
tosrees <tosrees@c7e7341f-4038-4d2c-49e7-fada537707ad>
2011-02-24 13:17:14 +0000

    * level editor basic frame work

---
+++ LevelEditor/LevelEditor/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LevelEditor
+{
+    public partial class LevelEditor : Form
+    {
+        public LevelEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBrowseSprite_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog.ShowDialog();
+        }
+
+        private void pbTileViewer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonBrowseTilesetFolder_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog.ShowDialog();
+        }
+    }
+}
