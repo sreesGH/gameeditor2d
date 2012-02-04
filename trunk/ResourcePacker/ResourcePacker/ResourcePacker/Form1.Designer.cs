ec6756be01dc7d39bf62cb5126b6f3cf3590606c
tosrees <tosrees@c7e7341f-4038-4d2c-49e7-fada537707ad>
2012-02-04 22:03:43 +0000

    + simple resource packer

---
+++ trunk/ResourcePacker/ResourcePacker/ResourcePacker/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace ResourcePacker
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsBtnNew = new System.Windows.Forms.ToolStripButton();
+            this.tsbtnOpen = new System.Windows.Forms.ToolStripButton();
+            this.tsBtnSave = new System.Windows.Forms.ToolStripButton();
+            this.tsBtnAdd = new System.Windows.Forms.ToolStripButton();
+            this.tsBtnRemove = new System.Windows.Forms.ToolStripButton();
+            this.tsBtanPack = new System.Windows.Forms.ToolStripButton();
+            this.dgViewPack = new System.Windows.Forms.DataGridView();
+            this.Index = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FileName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Size = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PackName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Compression = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.openFileDialogPackFile = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialogPackFile = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialogFile = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgViewPack)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.addToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(811, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exportToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.exportToolStripMenuItem.Text = "Pack";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem1,
+            this.deleteToolStripMenuItem});
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.addToolStripMenuItem.Text = "Options";
+            // 
+            // addToolStripMenuItem1
+            // 
+            this.addToolStripMenuItem1.Name = "addToolStripMenuItem1";
+            this.addToolStripMenuItem1.Size = new System.Drawing.Size(124, 22);
+            this.addToolStripMenuItem1.Text = "Add";
+            this.addToolStripMenuItem1.Click += new System.EventHandler(this.addToolStripMenuItem1_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.deleteToolStripMenuItem.Text = "Remove";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsBtnNew,
+            this.tsbtnOpen,
+            this.tsBtnSave,
+            this.tsBtnAdd,
+            this.tsBtnRemove,
+            this.tsBtanPack});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(811, 35);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // tsBtnNew
+            // 
+            this.tsBtnNew.AutoSize = false;
+            this.tsBtnNew.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsBtnNew.Image = ((System.Drawing.Image)(resources.GetObject("tsBtnNew.Image")));
+            this.tsBtnNew.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsBtnNew.Name = "tsBtnNew";
+            this.tsBtnNew.Size = new System.Drawing.Size(32, 32);
+            this.tsBtnNew.Text = "New";
+            this.tsBtnNew.Click += new System.EventHandler(this.tsBtnNew_Click);
+            // 
+            // tsbtnOpen
+            // 
+            this.tsbtnOpen.AutoSize = false;
+            this.tsbtnOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtnOpen.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnOpen.Image")));
+            this.tsbtnOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnOpen.Name = "tsbtnOpen";
+            this.tsbtnOpen.Size = new System.Drawing.Size(32, 32);
+            this.tsbtnOpen.Text = "Open";
+            this.tsbtnOpen.Click += new System.EventHandler(this.tsbtnOpen_Click);
+            // 
+            // tsBtnSave
+            // 
+            this.tsBtnSave.AutoSize = false;
+            this.tsBtnSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsBtnSave.Image = ((System.Drawing.Image)(resources.GetObject("tsBtnSave.Image")));
+            this.tsBtnSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsBtnSave.Name = "tsBtnSave";
+            this.tsBtnSave.Size = new System.Drawing.Size(32, 32);
+            this.tsBtnSave.Text = "Save";
+            this.tsBtnSave.Click += new System.EventHandler(this.tsBtnSave_Click);
+            // 
+            // tsBtnAdd
+            // 
+            this.tsBtnAdd.AutoSize = false;
+            this.tsBtnAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsBtnAdd.Image = ((System.Drawing.Image)(resources.GetObject("tsBtnAdd.Image")));
+            this.tsBtnAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsBtnAdd.Name = "tsBtnAdd";
+            this.tsBtnAdd.Size = new System.Drawing.Size(32, 32);
+            this.tsBtnAdd.Text = "Add";
+            this.tsBtnAdd.Click += new System.EventHandler(this.tsBtnAdd_Click);
+            // 
+            // tsBtnRemove
+            // 
+            this.tsBtnRemove.AutoSize = false;
+            this.tsBtnRemove.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsBtnRemove.Image = ((System.Drawing.Image)(resources.GetObject("tsBtnRemove.Image")));
+            this.tsBtnRemove.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsBtnRemove.Name = "tsBtnRemove";
+            this.tsBtnRemove.Size = new System.Drawing.Size(32, 32);
+            this.tsBtnRemove.Text = "Remove";
+            // 
+            // tsBtanPack
+            // 
+            this.tsBtanPack.AutoSize = false;
+            this.tsBtanPack.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsBtanPack.Image = ((System.Drawing.Image)(resources.GetObject("tsBtanPack.Image")));
+            this.tsBtanPack.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsBtanPack.Name = "tsBtanPack";
+            this.tsBtanPack.Size = new System.Drawing.Size(32, 32);
+            this.tsBtanPack.Text = "Pack";
+            this.tsBtanPack.Click += new System.EventHandler(this.tsBtanPack_Click);
+            // 
+            // dgViewPack
+            // 
+            this.dgViewPack.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgViewPack.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Index,
+            this.FileName,
+            this.Size,
+            this.PackName,
+            this.Compression});
+            this.dgViewPack.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgViewPack.Location = new System.Drawing.Point(0, 59);
+            this.dgViewPack.Name = "dgViewPack";
+            this.dgViewPack.Size = new System.Drawing.Size(811, 441);
+            this.dgViewPack.TabIndex = 2;
+            this.dgViewPack.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgViewPack_CellValueChanged);
+            this.dgViewPack.Enter += new System.EventHandler(this.dgViewPack_Enter);
+            this.dgViewPack.CellStateChanged += new System.Windows.Forms.DataGridViewCellStateChangedEventHandler(this.dgViewPack_CellStateChanged);
+            this.dgViewPack.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgViewPack_CellEndEdit);
+            this.dgViewPack.EditModeChanged += new System.EventHandler(this.dgViewPack_EditModeChanged);
+            // 
+            // Index
+            // 
+            this.Index.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.Index.HeaderText = "Index";
+            this.Index.Name = "Index";
+            this.Index.ReadOnly = true;
+            this.Index.Width = 58;
+            // 
+            // FileName
+            // 
+            this.FileName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.FileName.HeaderText = "File Name";
+            this.FileName.MinimumWidth = 200;
+            this.FileName.Name = "FileName";
+            // 
+            // Size
+            // 
+            this.Size.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Size.HeaderText = "Size";
+            this.Size.Name = "Size";
+            this.Size.ReadOnly = true;
+            this.Size.Width = 52;
+            // 
+            // PackName
+            // 
+            this.PackName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.PackName.HeaderText = "Pack Name";
+            this.PackName.Name = "PackName";
+            this.PackName.Width = 88;
+            // 
+            // Compression
+            // 
+            this.Compression.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.Compression.HeaderText = "Compression";
+            this.Compression.Name = "Compression";
+            this.Compression.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Compression.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Compression.Width = 92;
+            // 
+            // openFileDialogPackFile
+            // 
+            this.openFileDialogPackFile.Filter = "*.pak|*.pak";
+            this.openFileDialogPackFile.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialogPackFile_FileOk);
+            // 
+            // saveFileDialogPackFile
+            // 
+            this.saveFileDialogPackFile.Filter = "*.pak|*.pak";
+            this.saveFileDialogPackFile.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // openFileDialogFile
+            // 
+            this.openFileDialogFile.Filter = "All files|*.*";
+            this.openFileDialogFile.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialogFile_FileOk);
+            // 
+            // frmMain
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 500);
+            this.Controls.Add(this.dgViewPack);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Text = "Resource Packer";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmMain_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.frmMain_DragEnter);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgViewPack)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbtnOpen;
+        private System.Windows.Forms.ToolStripButton tsBtnSave;
+        private System.Windows.Forms.ToolStripButton tsBtnAdd;
+        private System.Windows.Forms.ToolStripButton tsBtanPack;
+        private System.Windows.Forms.ToolStripButton tsBtnRemove;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dgViewPack;
+        private System.Windows.Forms.OpenFileDialog openFileDialogPackFile;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogPackFile;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripButton tsBtnNew;
+        private System.Windows.Forms.OpenFileDialog openFileDialogFile;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Index;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FileName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Size;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PackName;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Compression;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+    }
+}
+
